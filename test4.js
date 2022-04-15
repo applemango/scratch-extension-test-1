0c895181d5892ebb91b8e37831e0d5e933bcae7e
applemango <85370582+applemango@users.noreply.github.com>
2022-04-15 17:28:48 +0900

    Update test4.js

--- test4.js
+++ test4.js
@@ -16,7 +16,7 @@ class Easing_main {
                         max: {
                             type: Scratch.ArgumentType.STRING,
                             defaultValue: '1'
-                        },
+                        }
                     }
                 }
             ]
@@ -26,11 +26,14 @@ class Easing_main {
         if(args.type == "easeInOutBack"){
             const c1 = 1.70158;
             const c2 = c1 * 1.525;
-            const x = args.x
+            const x = args.x;
 
             return x < 0.5
-                ? (pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
-                : (pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;    
+                ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+                : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;    
+        } else {
+            console.error("Easing type not found");
+            return null;
         }
     }
 }
